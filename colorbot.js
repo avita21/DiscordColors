052b71ebb7919c1ceca2a7383edb9906cf6cff0d
Avita <avita.sharma@tufts.edu>
2017-06-24 21:26:39 -0400

    concluded adding colors will take much longer

--- colorbot.js
+++ colorbot.js
@@ -2,15 +2,31 @@
 /* Discord Color Bot */
 
 const Discord = require("discord.js");
-console.log(process.env.DISCORD_KEY);
+const channel = "273497322250305546";
+const baseUrl = "https://discordapp.com/api";
+
 /* get the Discord Secret Bot Token as your environmental variable */
 const client  = new Discord.Client();
 const token   = "MzI4MjMxNjQ4NDcyNTk2NDgy.DDA5Sw.zIyHiMuwzJO59EYwDt6wi7M7ISk";
+
+
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
 client.on('ready', () => {
   console.log('I am ready!');
+//  console.log(client.channels);
+  send_color();
 });
+
+function send_color() {
+	options = new Discord.RichEmbed();
+	options.color = 8074292;
+	options.title = '*HELLO*';
+	options.url = 'https://discordapp.com';
+	console.log(options)
+	client.channels.get(channel).send("hello", {'embed': options});
+}
+
 
 // Create an event listener for messages
 client.on('message', message => {
@@ -22,6 +38,4 @@ client.on('message', message => {
 });
 
 // Log our bot in
-client.login(token);
-
 client.login(token);
